ad927b4f17b22b5759280337965ae58beb4e6e5e
Gilberto Romero-Cano <gilbertorcp712@gmail.com>
2026-08-01 16:10:13 -0600

    update build output hashing

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,15 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name]-[hash].js',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        assetFileNames: 'assets/[name]-[hash].[ext]'
+      }
+    }
+  },
   server: {
     port: 5173,
     proxy: {
